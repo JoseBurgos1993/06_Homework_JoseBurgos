0e4ba5d6a90093d05b2bfcd81245c227efbd4104
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-06-23 16:07:07 -0400

    Created framework for forecasts in html and js

--- Assets/script.js
+++ Assets/script.js
@@ -24,13 +24,41 @@ $("document").ready(function(){
                 // If query succeeded
                 if(e == true){
                     createButton(cityName);
-                    console.log(response);
 
                     // Do Data
+                    const today = response.list[0];
+                    const forecast1 = response.list[1];
+                    const forecast2 = response.list[2];
+                    const forecast3 = response.list[3];
+                    const forecast4 = response.list[4];
+                    const forecast5 = response.list[5];
+
+                    const UV = "xxx";
+                    $("#today").html(
+                        cityName + "(DATE)" + weatherSymbol() + "<br>" +
+                        "Temperature: " + today.main.temp + "&#8457 <br>" +
+                        "Humidity: " + today.main.humidity + "% <br>" +
+                        "Wind Speed: " + today.wind.speed + "MPH <br>" +
+                        "UV Index: " + UV
+                    );
+                    console.log(today);
+                    console.log(forecast1);
+                    console.log(forecast2);
+                    console.log(forecast3);
+                    console.log(forecast4);
+                    console.log(forecast5);
+
+                    //http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
                 }
             }
         );
     }
+
+    // Gets the correct weather symbol based on that day's weather
+    function weatherSymbol(){
+        return "&#9729";
+    }
+
     // Creates a new button for the search history area
     function createButton(cityName){
         const newButton = $("<button>");

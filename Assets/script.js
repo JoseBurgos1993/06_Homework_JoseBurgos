e0ee0e4db43aecf3182e5245e05ec83cf100d387
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-06-25 16:27:20 -0400

    Forecasts and buttons work. Need localstorage

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 // Javascript file
 // By Jose Burgos
+// https://en.wikipedia.org/wiki/Miscellaneous_Symbols
+
 
 $("document").ready(function(){
     //---- GLOBAL VARIABLES ----\\
@@ -23,41 +25,91 @@ $("document").ready(function(){
             }).then(function(response){
                 // If query succeeded
                 if(e == true){
-                    createButton(cityName);
 
                     // Do Data
                     const today = response.list[0];
+                    /*
                     const forecast1 = response.list[1];
                     const forecast2 = response.list[2];
                     const forecast3 = response.list[3];
                     const forecast4 = response.list[4];
                     const forecast5 = response.list[5];
+                    */
+                    for(let i=0; i<5; i++){
+                        const forecast = response.list[i+1];
+                        let temp1 = forecast.main.temp;
+                        temp1 = ((temp1 - 273.15) *1.8 + 32).toFixed(1);
+                        const humid = forecast.main.humidity;
+                        const icon1 = weatherSymbol(forecast.weather[0].icon);
 
-                    const UV = "xxx";
-                    $("#rightSide").css("display", "block");
-                    $("#today").html(
-                        "<h2>" + cityName + "(DATE)" + weatherSymbol() + "</h2><br>" +
-                        "Temperature: " + today.main.temp + "&#8457 <br>" +
-                        "Humidity: " + today.main.humidity + "% <br>" +
-                        "Wind Speed: " + today.wind.speed + "MPH <br>" +
-                        "UV Index: " + UV
-                    );
-                    console.log(today);
-                    console.log(forecast1);
-                    console.log(forecast2);
-                    console.log(forecast3);
-                    console.log(forecast4);
-                    console.log(forecast5);
 
-                    //http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+                    }
+                    let i = 1;
+                    $(".dayCast").each(function(){
+                        const forecast = response.list[i];
+                        i+=8;
+                        const temp = ((forecast.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+                        const humid = forecast.main.humidity;
+                        const icon = weatherSymbol(forecast.weather[0].icon);
+                        const date = forecast.dt_txt.substring(5,10);
+                        $(this).html(
+                            date + "<br>" +
+                            "<img src='" + icon + "'></img><br>" +
+                            "Temp: " + temp + "&#8457 <br>" +
+                            "Humid: " + humid + "%"
+                        );
+                    });
+                    
+                    //$("#currentDay").text(moment().format("MMMM Do YYYY"));
+                    const date = moment().format("(MMMM Do YYYY)");
+                    const temp = ((today.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+                    const humidity = today.main.humidity;
+                    const windspeed = today.wind.speed.toFixed(1);
+                    const lat = response.city.coord.lat;
+                    const lon = response.city.coord.lon;
+                    var UV = 0.0;
+                    const newURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,daily&appid=" + apikey;
+
+                    console.log("--------WEATHER SYMBOL--------");
+                    console.log(today.weather[0].main);
+                    console.log(weatherSymbol(today.weather[0].icon));
+                    const icon = weatherSymbol(today.weather[0].icon);
+                    console.log("------------------------------");
+                    
+                    $.ajax({url: newURL, method: "GET",}).then(function(newResponse){
+                        UV = newResponse.current.uvi;
+
+                        $("#rightSide").css("display", "block");
+                        $("#today").html(
+                            "<h2>" + cityName + " " + date + " " + " <img src='" + icon + "'></img>"
+                            + "</h2><br>" +
+                            "Temperature: " + temp   + "&#8457 <br>" +
+                            "Humidity: "    + humidity  + "% <br>" +
+                            "Wind Speed: "  + windspeed + " MPH <br>" +
+                            "UV Index: "    + UV
+                        );
+                        const lastCity = [cityName, temp, humidity, windspeed, UV, weatherSymbol];
+                        localStorage.setItem("lastCity", lastCity);
+
+                        /*
+                        console.log(today);
+                        console.log(forecast1);
+                        console.log(forecast2);
+                        console.log(forecast3);
+                        console.log(forecast4);
+                        console.log(forecast5);
+                        */
+                    });
                 }
             }
         );
     }
 
     // Gets the correct weather symbol based on that day's weather
-    function weatherSymbol(){
-        return "&#9729";
+    function weatherSymbol(code){
+        //http://openweathermap.org/img/wn/10d@2x.png
+        
+        return "http://openweathermap.org/img/wn/" + code + ".png";
     }
 
     // Creates a new button for the search history area
@@ -65,6 +117,13 @@ $("document").ready(function(){
         const newButton = $("<button>");
         newButton.text(cityName);
         newButton.addClass("cityBtn");
+
+        newButton.on("click", function(){
+            event.preventDefault();
+            const cityName = $(this).text();
+            getWeatherData(cityName);
+        });
+
         const newRow = $("<div>");
         newRow.addClass("row");
         newRow.append(newButton);
@@ -90,19 +149,41 @@ $("document").ready(function(){
 
     // Submit button event listener. Grabs city name from textfield. 
     // If no duplicate exists, runs getWeatherData()
+    
     $(".submit").on("click", function(){
         event.preventDefault();
-        const cityName = $("#cityName").val();
+        const cityName = $("#cityName").val().toUpperCase();
 
         if(!checkDuplicate(cityName)){
             getWeatherData(cityName);
+            createButton(cityName);
         }
     });
 
+
+    $(".clear").on("click", function(){
+        $(".btnList").empty();
+    })
+    /*
+    function cityClickEvent(){
+        event.preventDefault();
+        const cityName = $(this).text();
+        if(!checkDuplicate(cityName)){
+            getWeatherData(cityName);
+        }
+    }*/
     // City buttons exist as a search history. Clicking them will re-query that data
-    $(".cityBtn").on("click", function(){
-        getWeatherData($(this).text());
+    
+    $(document).on("click",".cityBtn", function(){
+        console.log("-----");
+        console.log($(this));
+        console.log($(this).children()[0].text());
+        console.log("-----");
+        if(!checkDuplicate($(this).children(0).text())){
+            getWeatherData($(this).children(0).text());
+        }
     });
+    
 
     //---- MAIN FUNCTION ----\\
     function main(){
